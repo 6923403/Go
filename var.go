d1bc0bc6b81c032456a9e819660fb6b8c4ac9388
Then <5435@163.com>
2020-04-18 22:05:54 +0800

    变量

---
+++ var.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Hw"
+	a2 := "ss"
+
+	fmt.Println(a)
+	fmt.Println(a2)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var ce bool
+	fmt.Println(ce)
+}
